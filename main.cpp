ba8caf58e9450c65319053d96b0f813cdc061d95
Parth Patel <psyrotix@gmail.com>
2016-09-11 10:48:12 -0400

    initial push

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  CS280 proj 1
+//
+//  Created by parth patel on 9/11/16.
+//
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  CS280 proj 1
+//
+//  Created by parth patel on 9/11/16.
+//
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
