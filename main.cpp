6071d7a8c45855865d82e1da45f5b08df165113c
Parth Patel <psyrotix@gmail.com>
2016-09-15 05:41:34 -0400

    Duplicate Space Removal

--- main.cpp
+++ main.cpp
@@ -9,25 +9,64 @@
 #include "main.hpp"
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+
+string line;
+string output;
+string rawInput = "";
+int lineCount = 0;
+
+
+string removeSpace(string in) {
+    int isSpace = 0;
+    string out = "";
+    for(int i = 0; i < in.length(); i++){
+        if(in[i] == ' '){
+            if (isSpace == 1) continue;
+            else {
+                out += in[i];
+                isSpace = 1;
+            }
+        } else {
+            out += in[i];
+            isSpace = 0;
+        }
+    }
+    return out;
+}
+
 int main (int argc, const char * argv[]) {
-    cout << argv[1];
+    cout << "Reading file: " << argv[1];
     cout << '\n';
-    char * memblock;
     
-    ifstream file (argv[1], ios::in|ios::binary|ios::ate);
+    
+    int lineSize = 40;
+    ifstream file (argv[1]);
+    
+    size_t pos = 0;
+    
+    string s;
+    
     if (file.is_open())
     {
-        int size = file.tellg();
-        memblock = new char[size];
-        file.seekg (0, ios::beg);
-        file.read (memblock, size);
+        
+        while ( getline (file,line) )
+        {
+            rawInput += line + ' ';
+            
+            
+//            cout << line.length() << ' ';
+            
+            lineCount++;
+        }
+        string test = removeSpace("stuff     things haha");
+        
+        cout << test;
         file.close();
-        
-        cout << memblock;
-        
-        delete[] memblock;
     }
     else cout << "Unable to open file";
+    
     return 0;
 }
