892913e09aa986cf1edf626200558af82031f7ad
Parth Patel <psyrotix@gmail.com>
2016-09-14 22:21:20 -0400

    Reads file on input

--- main.cpp
+++ main.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "main.hpp"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main (int argc, const char * argv[]) {
+    cout << argv[1];
+    cout << '\n';
+    char * memblock;
+    
+    ifstream file (argv[1], ios::in|ios::binary|ios::ate);
+    if (file.is_open())
+    {
+        int size = file.tellg();
+        memblock = new char[size];
+        file.seekg (0, ios::beg);
+        file.read (memblock, size);
+        file.close();
+        
+        cout << memblock;
+        
+        delete[] memblock;
+    }
+    else cout << "Unable to open file";
+    return 0;
+}
