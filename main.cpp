a322a593865591bbc9b5b97e53267f76a12a6de3
Parth Patel <psyrotix@gmail.com>
2016-09-20 11:38:24 -0400

    New paragraph detection

--- main.cpp
+++ main.cpp
@@ -10,63 +10,94 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 using namespace std;
 
 
 string line;
 string output;
 string rawInput = "";
-int lineCount = 0;
+
+regex newLineToken ("^\\.ll N");
+regex dotLineToken ("^\\..+");
+regex findWords ("\\S+");
+smatch wordResults;
 
 
-string removeSpace(string in) {
-    int isSpace = 0;
-    string out = "";
-    for(int i = 0; i < in.length(); i++){
-        if(in[i] == ' '){
-            if (isSpace == 1) continue;
-            else {
-                out += in[i];
-                isSpace = 1;
-            }
-        } else {
-            out += in[i];
-            isSpace = 0;
-        }
+int isNewParagraph = 0;
+int lineNum = 0;
+int wordCount = 0;
+int spaceCount = 0;
+int lineLength = 60;
+
+
+string getWords(string in){
+    int k = 0;
+    string currentWord =  "";
+    regex_search(in, wordResults, findWords);
+//    for(int i = 0; i < in.length(); i++){
+////        if((in[i] == "<") && (i < in.length() - 2)){
+////            if((in[i+1] == "p") && (in[i+2]==">")){
+////                
+////            }
+////        }
+//        if(in[i] == " "){
+//            currentWord = "";
+//            spaceCount++;
+//            k++;
+//            continue;
+//        }
+//    }
+    for(int i=0; i< wordResults.length(); i++){
+        cout << wordResults[i];
     }
-    return out;
+    return in;
 }
 
+//string finalOutput(string in){
+//    
+//}
+
 int main (int argc, const char * argv[]) {
-    cout << "Reading file: " << argv[1];
-    cout << '\n';
+    //Check if user has input file name...
+    if(argc < 2){
+        cout << "Missing filename.\n\n";
+        return 0;
+    }
+    //fail if lineLength is not valid (must be no less than 10)
+    if(lineLength < 10){
+        cout << "Line length is too small.\n\n";
+        return 0;
+    }
     
+    cout << "\nReading file: " << argv[1];
+    cout << "\n\n";
     
-    int lineSize = 40;
+    //Start reading the file...
     ifstream file (argv[1]);
     
-    size_t pos = 0;
-    
-    string s;
-    
-    if (file.is_open())
-    {
-        
-        while ( getline (file,line) )
-        {
-            rawInput += line + ' ';
-            
-            
-//            cout << line.length() << ' ';
-            
-            lineCount++;
+    //Print original data and unformat most of the input.
+    if (file.is_open()) {
+        while ( getline (file,line) ) {
+            if (regex_match(line,newLineToken) && (isNewParagraph == 0)) {
+                rawInput += "<p>";
+                isNewParagraph = 1;
+                continue;
+            } else if(regex_match(line,newLineToken) && (isNewParagraph == 1)) {
+                continue;
+            } else if (regex_match(line,dotLineToken))  {
+                continue;
+            } else {
+                rawInput += line + ' ';
+                isNewParagraph = 0;
+            }
         }
-        string test = removeSpace("stuff     things haha");
-        
-        cout << test;
         file.close();
-    }
-    else cout << "Unable to open file";
+    } else cout << "Unable to open file";
+    getWords(rawInput);
+    
+    //Function to separate words into an array and count spaces.
+    
     
     return 0;
 }
